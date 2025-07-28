0181eb5d000608a502d9f37ec42060200b351007
Paul123111 <116811799+Paul123111@users.noreply.github.com>
2025-07-28 16:44:01 +0100

    hi (#16)

--- .github/scripts/promote_branch.sh
+++ .github/scripts/promote_branch.sh
@@ -151,7 +151,7 @@ do
   curl -s   -H 'Authorization: token  '"$token"  'https://api.github.com/search/issues?q=sha:'"$COMMIT" | jq -r '.items[]
     | select(.repository_url=="https://api.github.com/repos/'"$ORG"'/'"$REPO"'")
     | .pull_request | select(.merged_at!=null) | .html_url'
-  LABELS=($(curl -s   -H 'Authorization: token  '"$token"  'https://api.github.com/search/issues?q=sha:'"$COMMIT" | jq -r '.items[]
+  LABELS=($(curl -s   -H 'Authorization: token  '"$token"  'https://api.github.com/search/issues?q=sha:'"$COMMIT" | jq '.items[]
     | select(.repository_url=="https://api.github.com/repos/'"$ORG"'/'"$REPO"'")
     | .labels[].name'))
   LABELS="$(echo "${LABELS[@]/%/,}")"

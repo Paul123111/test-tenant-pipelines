480033ce8c3a3660733f6d65389fb166b47494d1
Paul Fitzgerald <pafitzge@redhat.com>
2025-07-28 12:16:18 +0100

    optimised unoptimised code

--- .github/scripts/promote_branch.sh
+++ .github/scripts/promote_branch.sh
@@ -151,9 +151,7 @@ do
   curl -s   -H 'Authorization: token  '"$token"  'https://api.github.com/search/issues?q=sha:'"$COMMIT" | jq -r '.items[]
     | select(.repository_url=="https://api.github.com/repos/'"$ORG"'/'"$REPO"'")
     | .pull_request | select(.merged_at!=null) | .html_url'
-  curl -s   -H 'Authorization: token  '"$token"  'https://api.github.com/search/issues?q=sha:'"$COMMIT" | jq -r '.items[]
-    | select(.repository_url=="https://api.github.com/repos/'"$ORG"'/'"$REPO"'")
-    | .pull_request | select(.merged_at!=null) | .labels.[].name'
+  echo $(curl -s   -H 'Authorization: token  '"$token"  'https://api.github.com/search/issues?q=sha:'"$COMMIT")
   git show --oneline --no-patch "$COMMIT"
 done
 

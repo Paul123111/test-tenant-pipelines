4c2c06b01e924a662b05c300210293b1ab08e284
Paul Fitzgerald <pafitzge@redhat.com>
2025-07-28 13:16:09 +0100

    hi

--- .github/scripts/promote_branch.sh
+++ .github/scripts/promote_branch.sh
@@ -151,9 +151,10 @@ do
   curl -s   -H 'Authorization: token  '"$token"  'https://api.github.com/search/issues?q=sha:'"$COMMIT" | jq -r '.items[]
     | select(.repository_url=="https://api.github.com/repos/'"$ORG"'/'"$REPO"'")
     | .pull_request | select(.merged_at!=null) | .html_url'
-  curl -s   -H 'Authorization: token  '"$token"  'https://api.github.com/search/issues?q=sha:'"$COMMIT" | jq -r '.items[]
+  LABELS=$(curl -s   -H 'Authorization: token  '"$token"  'https://api.github.com/search/issues?q=sha:'"$COMMIT" | jq -r '.items[]
     | select(.repository_url=="https://api.github.com/repos/'"$ORG"'/'"$REPO"'")
-    | .labels[].name'
+    | .labels[].name')
+  echo "PR Labels: ${LABELS[@]}"
   git show --oneline --no-patch "$COMMIT"
 done
 
